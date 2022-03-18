073924ef3e60e6eadd33128dea822ff16ddb4de1
Hélène Martin <helenemartin@users.noreply.github.com>
2022-03-18 10:40:51 +0000

    Add fluidity to desktop navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -28,6 +28,7 @@ const Header = () => {
           <NavLink href="/sale">Sale</NavLink>
           <NavLink href="/new">New&nbsp;Releases</NavLink>
           <NavLink href="/men">Men</NavLink>
+          <NavLink href="/men">Men</NavLink>
           <NavLink href="/women">Women</NavLink>
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
@@ -62,7 +63,8 @@ const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
-  height: 72px;
+  /* height: 72px; */
+  overflow: auto;
   border-bottom: 1px solid ${COLORS.gray[300]};
   border-top: 4px solid ${COLORS.gray[900]};
   @media ${QUERIES.tabletAndSmaller} {
@@ -77,8 +79,9 @@ const MainHeader = styled.div`
 
 const DesktopNav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(1rem, 9.2vw - 4.5rem, 3.5rem);
   margin: 0px 48px;
+ 
   @media ${QUERIES.tabletAndSmaller} {
     display: none;
   }
